1f4299ef4cd6cd61c091be45c0e3848d67701f5b
joshmalk <joshmalk03@gmail.com>
2023-10-26 12:15:38 -0400

    Working demo: needs a few tweaks

--- src/PolarisChip.js
+++ src/PolarisChip.js
@@ -5,22 +5,34 @@ export class PolarisChip extends LitElement {
     return {
       name: { type: String },
       link: {type: String},
-      active: {type: Boolean, reflect: true}
+      active: {type: Boolean, reflect: true},
+      filled: {type: Boolean, reflect: true}
     };
   }
 
   static get styles() {
     return css`
       :host {
-        display: inline-block;
+        display: inline-flex;
         margin: 0 12px 12px 0;
+        justify-content: center;
+        padding: 1rem 1rem;
+        text-align: center
       }
 
         .link:focus,
         .link:hover,
         :host([active]) .link{
-          background-color: #7ac6ff;
-          border: 2px solid #8acdff;
+          background-color: #005fa9;
+          border: 2px solid #005fa9;
+          border-radius: 2px;
+          color: #ffffff;
+          cursor: pointer;
+        }
+
+        :host([filled]) .link:hover{
+          background-color: black;
+          border: 2px solid #005fa9;
           border-radius: 2px;
           color: #ffffff;
           cursor: pointer;
@@ -30,7 +42,7 @@ export class PolarisChip extends LitElement {
           font-weight:bold;
           text-decoration:none;
           padding: 0.75rem 1rem 0.75rem 1.5rem;
-          border: 2px solid ##005fa9;
+          border: 2px solid #005fa9;
           color: #005fa9;
           font-size: 16px;
         }
